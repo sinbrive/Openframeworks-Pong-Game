d897049dfec8c80b8023730706406e2eaa43bb55
sinbrive <21102151+sinbrive@users.noreply.github.com>
2022-11-23 14:57:02 +0100

    Update paddle.cpp

--- src/paddle.cpp
+++ src/paddle.cpp
@@ -14,11 +14,11 @@ Paddle::Paddle(float _x, float _y, float _w, float _h) {
 void Paddle::update() {
 
 	(x > ofGetWidth()/2) ? y += step: y = ofGetMouseY();  
-	if (y > ofGetHeight() - 10 - 40) {
-		y = ofGetHeight() - 10 - 40;
+	if (y > ofGetHeight() - 10 - h/2) {
+		y = ofGetHeight() - 10 - h/2;
 	}
-	else if (y < 10 + 40) {
-		y = 10 + 40;
+	else if (y < 10 + h/2) {
+		y = 10 + h/2;
 	}
 }
 
